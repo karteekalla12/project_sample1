9aa8a67d0245158aeef76b87b8f2befd0108c2d9
karteek <kartheekalla66@gmail.com>
2025-07-18 00:38:06 -0400

    commiting 111.py and abc.py

--- 111.py
+++ 111.py
@@ -1,9 +1,5 @@
 x=1
-<<<<<<< HEAD
-y=2
-=======
 y=3
->>>>>>> origin/main
 if x>y:
     print(x)
 else:
--- abc.py
+++ abc.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
 print("hello123")
-<<<<<<< HEAD
-asdfedvdd
-=======
-ssddsffdprint("hello123")
->>>>>>> 2f6609de7d277f19e7d90334de7c6f305de1632b
-=======
 print("abc")
->>>>>>> origin/main
+
